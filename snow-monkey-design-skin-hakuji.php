1d3d824be76b6464a4e615dea1404e8893e88def
Takashi Kitajima <inc@2inc.org>
2018-11-02 19:51:07 +0900

    0.1.5

--- snow-monkey-design-skin-hakuji.php
+++ snow-monkey-design-skin-hakuji.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: [ Snow Monkey Design Skin ] Hakuji
  * Description: A design skin of the Snow Monkey.
- * Version: 0.1.4
+ * Version: 0.1.5
  *
  * @package snow-monkey-design-skin-hakuji
  * @author inc2734
